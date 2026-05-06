0e922c4921d3215ffedb10cff71bdb648e2091ae
Gustavo <162048765+otavioing@users.noreply.github.com>
2026-05-05 21:31:27 -0300

    feat: implement UserController with authentication methods and add getUserToken helper

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const createUserToken = require('../helpers/create user token')
 const getToken = require('../helpers/get-tokens')
+const getUserByToken = require('../helpers/get-user-by-token')
 
 module.exports = class UserController {
     static async register(req, res) {
@@ -134,9 +135,50 @@ module.exports = class UserController {
     }
 
     static async editUser(req, res){
-        res.status(200).json({
-            mensage: 'Usuário editado com sucesso!'
-        })
+        const id = req.params.id
+
+
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        const { name, phone, password, confirmpassword } = req.body
+        let image = ''
+
+        if (!name) {
+            res.status(422).json({ message: 'O nome é obrigatório' })
+            return
+        }
+        if (!email) {
+            res.status(422).json({ message: 'O email é obrigatório' })
+            return
+        }
+        if (!phone) {
+            res.status(422).json({ message: 'O telefone é obrigatório' })
+            return
+        }
+        if (!password) {
+            res.status(422).json({ message: 'A senha é obrigatória' })
+            return
+        }
+        if (!confirmpassword) {
+            res.status(422).json({ message: 'A confirmação de senha é obrigatória' })
+            return
+        }
+        if (password !== confirmpassword) {
+            res.status(422).json({ message: 'As senhas não conferem' })
+            return
+        }
+
+        const userExists = await User.findOne({ email: email})
+
+        if (userExists.email === email && userExists) {
+            res.status(422).json({ message: 'Existe um problema de chave email com a edição!'})
+            return
+        }
+
+        const salt = await bcrypt.genSalt(12)
+        const passwordHash = await bcrypt.hash(password, salt)
+        
         
     }
 }
